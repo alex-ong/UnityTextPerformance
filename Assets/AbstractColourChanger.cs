59d041a8c6ba1954f200b08ac692a3ed3b144a93
alex-ong <the.onga@gmail.com>
2018-02-14 08:34:49 +1100

    Added colourText changer and implemented for canvas

---
+++ Assets/AbstractColourChanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AbstractColourChanger : MonoBehaviour {
+    protected abstract Color getColor();
+    protected abstract void SetColor(Color c);
+
+    float timer = 5.0f;
+    protected float getPerc()
+    {
+        timer -= Time.deltaTime;
+        timer = Mathf.Clamp01(timer);
+        return 1.0f - timer + 0.1f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Color c = getColor();
+        c.a = getPerc();
+        SetColor(c);
+	}
+}
---
+++ Assets/TextCanvasColourChanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class TextCanvasColourChanger : AbstractColourChanger {
+    private Text t;
+    protected override Color getColor()
+    {
+        return t.color;
+    }
+
+    protected override void SetColor(Color c)
+    {
+        t.color = c;
+    }
+    
+    protected void Awake()
+    {
+        t = this.GetComponent<Text>();
+    }
+}
